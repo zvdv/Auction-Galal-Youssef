7abdb6583202539bc96a618233aaa4bc17d8e104
zvdv <156561301+zvdv@users.noreply.github.com>
2024-06-07 22:14:19 -0700

    Updated deprecated Solidity syntax in Auction Contract.

--- Auction Contract.sol
+++ Auction Contract.sol
@@ -7,12 +7,12 @@ contract Pedersen {
     uint private hX = 3184834430741071145030522771540763108892281233703148152311693391954704539228;
     uint private hY = 1405615944858121891163559530323310827496899969303520166098610312148921359100;
     function Commit(uint b, uint r) public returns (uint cX, uint cY) {
-        var (cX1, cY1) = ecMul(b, gX, gY);
-        var (cX2, cY2) = ecMul(r, hX, hY);
+        (uint cX1, uint cY1) = ecMul(b, gX, gY);
+        (uint cX2, uint cY2) = ecMul(r, hX, hY);
         (cX, cY) = ecAdd(cX1, cY1, cX2, cY2);
     }
     function Verify(uint b, uint r, uint cX, uint cY) public returns (bool) {
-        var (cX2, cY2) = Commit(b,r);
+        (uint cX2, uint cY2) = Commit(b,r);
         return cX == cX2 && cY == cY2;
     }
     function CommitDelta(uint cX1, uint cY1, uint cX2, uint cY2) public returns (uint cX, uint cY) {
@@ -84,7 +84,7 @@ contract Auction {
     address public winner;
     uint public highestBid;    
     //Constructor = Setting all Parameters and auctioneerAddress as well
-    function Auction(uint _bidBlockNumber, uint _revealBlockNumber, uint _winnerPaymentBlockNumber, uint _maxBiddersCount, uint _fairnessFees, string _auctioneerRSAPublicKey, address pedersenAddress, uint8 k, bool _testing) public payable {
+    constructor(uint _bidBlockNumber, uint _revealBlockNumber, uint _winnerPaymentBlockNumber, uint _maxBiddersCount, uint _fairnessFees, string _auctioneerRSAPublicKey, address pedersenAddress, uint8 k, bool _testing) public payable {
         require(msg.value >= _fairnessFees);
         auctioneerAddress = msg.sender;
         bidBlockNumber = block.number + _bidBlockNumber;
@@ -136,11 +136,10 @@ contract Auction {
             }
         states = VerificationStates.Verify;
     }
-    
     function ZKPVerify(uint[] response, uint[] deltaResponses) public challengeByAuctioneer {
         require(states == VerificationStates.Verify || states == VerificationStates.VerifyDelta);
         uint8 count =0;
-        uint hash = uint(block.blockhash(challengeBlockNumber));
+        uint hash = uint(blockhash(challengeBlockNumber));
         mask =1;
         uint i=0;
         uint j=0;
